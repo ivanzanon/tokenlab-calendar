5420b120d538135e6575985060417a3325f2a66c
Ivan Zanon <ivanzanon@gmail.com>
2020-05-23 16:12:00 -0300

    Creating React Project

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+/**
+ * @author Ivan Zanon
+ * 
+ * @description interface for the TokenLab Calendar API
+ * 
+ */
+
+ import axios from 'axios';
+
+ const api = axios.create({
+    baseURL: "http://localhost:3002/api"
+})
+
+export default api_o_bosque;
